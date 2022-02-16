9c5bf4cef2db61cdcbe9cf3f52e8468aca109c18
Vinh <vinh.bui0101@gmail.com>
2022-02-16 11:43:28 -0800

    Complete button functionalities and rules

---
+++ script.js
@@ -0,0 +1,26 @@
+const display = document.getElementById("current-out");
+const prevDisplay = document.getElementById("Previous-out");
+
+function action(obj){
+  var pushed = obj.innerHTML;
+  
+  if(pushed == '='){
+    display.innerHTML = eval(display.innerHTML);
+    prevDisplay.innerHTML = display.innerHTML;
+  }else if (pushed == 'CLEAR'){
+    display.innerHTML = '0';
+  }else if(pushed == 'DELETE'){
+    currentNum = display.innerHTML.length - 1;
+    display.innerHTML = display.innerHTML.replace(display.innerHTML[currentNum],'');
+    if(display.innerHTML.length == '0'){
+      display.innerHTML = 0;
+    }
+  }else{
+    if(display.innerHTML == '0'){
+      display.innerHTML = pushed;
+    }else{
+      display.innerHTML += pushed;
+    }
+  }
+}
+
